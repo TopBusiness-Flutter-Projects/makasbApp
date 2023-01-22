688e482c6164f83d33ce8be7f76d0ed8850f53d3
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2023-01-22 13:30:48 +0200

    ch

--- lib/screens/loginPage/login_page.dart
+++ lib/screens/loginPage/login_page.dart
@@ -25,14 +25,14 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(backgroundColor: AppColors.white, body: buildBodySection());
   }
 
- Widget buildBodySection() {
+  Widget buildBodySection() {
     LoginCubit cubit = BlocProvider.of<LoginCubit>(context);
 
     return BlocListener<LoginCubit, LoginState>(listener: (context, state) {
       print("Status=>${state}");
       if (state is OnSignUp) {
         Fluttertoast.showToast(
-            msg:'Invaild User'.tr(), // message
+            msg: 'Invaild User'.tr(), // message
             toastLength: Toast.LENGTH_SHORT, // length
             gravity: ToastGravity.BOTTOM, // location
             timeInSecForIosWeb: 1 // duration
@@ -42,234 +42,240 @@ class _LoginPageState extends State<LoginPage> {
       }
     },
         child: LayoutBuilder(builder: (context, constraints) {
-      return ListView(children: [
-        const SizedBox(
-          height: 56.0,
-        ),
-        _buildLogoSection(),
-        _buildLoginSection(cubit)
-      ]);
-    }));
+          return ListView(children: [
+            const SizedBox(
+              height: 56.0,
+            ),
+            _buildLogoSection(),
+            _buildLoginSection(cubit)
+          ]);
+        }));
   }
 
- Widget _buildLogoSection() {
+  Widget _buildLogoSection() {
     return
 
       Container(
-      alignment: Alignment.topCenter,
-      width: 220.0,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            '${AppConstant.localImagePath}logo.png',
-            width: 170.0,
-            height: 60,
-            alignment: Alignment.center,
-          ),
-          SizedBox(
-            height: 64.0,
-          ),
-        ],
-      ),
-    );
+        alignment: Alignment.topCenter,
+        width: 220.0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              '${AppConstant.localImagePath}logo.png',
+              width: 170.0,
+              height: 60,
+              alignment: Alignment.center,
+            ),
+            SizedBox(
+              height: 64.0,
+            ),
+          ],
+        ),
+      );
   }
 
   Widget _buildLoginSection(LoginCubit cubit) {
     return BlocBuilder<LoginCubit, LoginState>(
         builder: (context, state) {
-         return Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Row(
+          return Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
               children: [
-                Text(
-                  'welcome_back'.tr(),
-                  style: const TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.black),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 40.0,
-            ),
-            Row(
-              children: [
-                AppWidget.svg('email.svg', AppColors.colorPrimary, 15.0, 15.0),
-                SizedBox(width: 5), // give it width
+                Row(
+                  children: [
+                    Text(
+                      'welcome_back'.tr(),
+                      style: const TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.black),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                Row(
+                  children: [
+                    AppWidget.svg(
+                        'email.svg', AppColors.colorPrimary, 15.0, 15.0),
+                    SizedBox(width: 5), // give it width
 
-                Text(
-                  'email'.tr(),
-                  style: const TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Container(
-                height: 56.0,
-                decoration: BoxDecoration(
-                    color: AppColors.grey8,
-                    borderRadius: BorderRadius.circular(16.0)),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: TextFormField(
-                    maxLines: 1,
-                    autofocus: false,
-                    cursorColor: AppColors.colorPrimary,
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.next,
-                    onChanged: (data) {
-                      cubit.loginModel.email = data;
-                      cubit.checkValidLoginData();
-                    },
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'email'.tr(),
-                        hintStyle: const TextStyle(
-                            color: AppColors.grey1, fontSize: 14.0)),
-                  ),
-                )),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                AppWidget.svg('lock.svg', AppColors.colorPrimary, 15.0, 15.0),
-                SizedBox(width: 5), // give it width
-
-                Text(
-                  'password'.tr(),
-                  style: const TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Container(
-                height: 56.0,
-                decoration: BoxDecoration(
-                    color: AppColors.grey8,
-                    borderRadius: BorderRadius.circular(16.0)),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: TextFormField(
-                    maxLines: 1,
-                    autofocus: false,
-                    obscureText: cubit.ishidden,
-                    cursorColor: AppColors.colorPrimary,
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.text,
-                    onChanged: (data) {
-                      cubit.loginModel.password = data;
-                      cubit.checkValidLoginData();
-                    },
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'password'.tr(),
-
-                        hintStyle: const TextStyle(
-                            color: AppColors.grey1, fontSize: 14.0),
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            cubit.hide();
-                          },
-                          child: cubit.ishidden
-                              ? const Icon(
-                            Icons.visibility,
-                          )
-                              : const Icon(Icons.visibility_off),
-                        )),
-                  ),
-                )),
-            Row(
-              children: [
-                // give it width
-                InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(
-                          context, AppConstant.resetPasswordRoute);
-                    },
-                    child: Text(
-                      'forgotpassword'.tr(),
+                    Text(
+                      'email'.tr(),
                       style: const TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.normal,
-                          color: AppColors.colorPrimary),
+                          color: AppColors.black),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Container(
+                    height: 56.0,
+                    decoration: BoxDecoration(
+                        color: AppColors.grey8,
+                        borderRadius: BorderRadius.circular(16.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: TextFormField(
+                        maxLines: 1,
+                        autofocus: false,
+                        cursorColor: AppColors.colorPrimary,
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        onChanged: (data) {
+                          cubit.loginModel.email = data;
+                          cubit.checkValidLoginData();
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'email'.tr(),
+                            hintStyle: const TextStyle(
+                                color: AppColors.grey1, fontSize: 14.0)),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    AppWidget.svg(
+                        'lock.svg', AppColors.colorPrimary, 15.0, 15.0),
+                    SizedBox(width: 5), // give it width
+
+                    Text(
+                      'password'.tr(),
+                      style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                          color: AppColors.black),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Container(
+                    height: 56.0,
+                    decoration: BoxDecoration(
+                        color: AppColors.grey8,
+                        borderRadius: BorderRadius.circular(16.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: TextFormField(
+                        maxLines: 1,
+                        autofocus: false,
+                        obscureText: cubit.ishidden,
+                        cursorColor: AppColors.colorPrimary,
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.text,
+                        onChanged: (data) {
+                          cubit.loginModel.password = data;
+                          cubit.checkValidLoginData();
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'password'.tr(),
+
+                            hintStyle: const TextStyle(
+                                color: AppColors.grey1, fontSize: 14.0),
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                cubit.hide();
+                              },
+                              child: cubit.ishidden
+                                  ? const Icon(
+                                Icons.visibility,
+                              )
+                                  : const Icon(Icons.visibility_off),
+                            )),
+                      ),
+                    )),
+                Row(
+                  children: [
+                    // give it width
+                    InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(
+                              context, AppConstant.resetPasswordRoute);
+                        },
+                        child: Text(
+                          'forgotpassword'.tr(),
+                          style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.normal,
+                              color: AppColors.colorPrimary),
+                        ))
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: BlocBuilder<LoginCubit, LoginState>(
+                    builder: (context, state) {
+                      bool isValid = cubit.isLoginValid;
+                      if (state is OnLoginVaildFaild) {
+                        isValid = false;
+                      } else if (state is OnLoginVaild) {
+                        isValid = true;
+                      } else if (state is OnError) {}
+                      return
+                        MaterialButton(
+                          onPressed: isValid
+                              ? () {
+                            cubit.login(context);
+                            /*showConfirmCodeDialog();*/
+                            //Navigator.pushNamed(context, AppConstant.pageUserSignUpRoleRoute);
+                          }
+                              : null,
+                          height: 56.0,
+                          disabledColor: AppColors.grey4,
+                          child: Text(
+                            'login'.tr(),
+                            style: TextStyle(color: AppColors.white,
+                                fontSize: 16.0),
+                          ),
+                          color: isValid ? AppColors.colorPrimary : AppColors
+                              .grey4,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                        );
+                    },
+                  ),
+                ),
+                SizedBox(height: 100),
+                RichText(
+                    text: TextSpan(
+                      style: TextStyle(color: AppColors.black),
+                      /*defining default style is optional */
+
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'dont_have_account?'.tr(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16)),
+                        TextSpan(
+                            text: 'login'.tr(),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.of(context)
+                                    .pushNamed(AppConstant.pageSignupRoute);
+                              },
+                            style: TextStyle(
+                                color: AppColors.colorPrimary,
+                                decoration: TextDecoration.underline,
+                                fontSize: 16)),
+                      ],
                     ))
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: BlocBuilder<LoginCubit, LoginState>(
-                builder: (context, state) {
-                  bool isValid = cubit.isLoginValid;
-                  if (state is OnLoginVaildFaild) {
-                    isValid = false;
-                  } else if (state is OnLoginVaild) {
-                    isValid = true;
-                  } else if (state is OnError) {}
-                  return
-                    MaterialButton(
-                    onPressed: isValid
-                        ? () {
-                      cubit.login(context);
-                      /*showConfirmCodeDialog();*/
-                      //Navigator.pushNamed(context, AppConstant.pageUserSignUpRoleRoute);
-                    }
-                        : null,
-                    height: 56.0,
-                    disabledColor: AppColors.grey4,
-                    child: Text(
-                      'login'.tr(),
-                      style: TextStyle(color: AppColors.white, fontSize: 16.0),
-                    ),
-                    color: isValid ? AppColors.colorPrimary : AppColors.grey4,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0)),
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 100),
-            RichText(
-                text: TextSpan(
-                  style: TextStyle(color: AppColors.black),
-                  /*defining default style is optional */
-
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: 'dont_have_account?'.tr(),
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                    TextSpan(
-                        text: 'login'.tr(),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.of(context)
-                                .pushNamed(AppConstant.pageSignupRoute);
-                          },
-                        style: TextStyle(
-                            color: AppColors.colorPrimary,
-                            decoration: TextDecoration.underline,
-                            fontSize: 16)),
-                  ],
-                ))
-          ],
-        ),
-      );});
-    }
+          );
+        });
   }
+}
 
 
 
